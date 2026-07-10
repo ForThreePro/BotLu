2fb13ecdeb71fa33c14f499603b0121abcf72d7d
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 21:03:21 -0500

    Actualizar sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -3,10 +3,10 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (!/webp|image|video/g.test(mime)) return m.reply(`Responde a una imagen, video o gif para convertirlo en sticker.`)
+    if (!/webp|image|video/g.test(mime)) return m.reply(`⚡ *RAYO PREM* ➔ Responde a una *imagen, video o gif* para convertirlo en sticker.`) // Cambiado
     let img = await q.download()
-    let stiker = await sticker(img, false, 'Box Bot MD', 'Barboza')
-    conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    let stiker = await sticker(img, false, 'Team Nightwish', 'Whois Yallico') // Cambiado marca
+    conn.sendFile(m.chat, stiker, 'sticker.webp', '⚡ *Rayo Prem* | Sticker creado', m) // Cambiado caption
 }
 handler.help = ['s']
 handler.tags = ['sticker']
