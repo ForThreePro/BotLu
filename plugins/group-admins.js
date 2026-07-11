9bd8045a0e6bdcc5844d4fcf7f01dc5607db9613
YALLICO OWNER <yallico2024@gmail.com>
2026-07-11 14:25:03 -0500

    Actualizar group-admins.js

--- plugins/group-admins.js
+++ plugins/group-admins.js
@@ -1,10 +1,10 @@
 const handler = async (m, { conn, command }) => {
   if (!m.mentionedJid[0] &&!m.quoted) {
-    let texto = `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+    let texto = `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
 │ 🛡️ *CONTROL DE ADMIN*
 │
-│ ⚡ *Menciona o responde al usuario*
-│ 🌙 *para ${command === 'promote' || command === 'promover' || command === 'daradmin'? 'promover' : 'degradar'} como administrador*
+│ 😼 *Menciona o responde al usuario*
+│ 🐾 *para ${command === 'promote' || command === 'promover' || command === 'daradmin'? 'promover' : 'degradar'} como administrador*
 ╰─────────────────❒`
     return m.reply(texto, m.chat, { mentions: conn.parseMention(texto) })
   }
@@ -13,21 +13,21 @@ const handler = async (m, { conn, command }) => {
   let action = /^(promote|promover|daradmin)$/i.test(command)? 'promote' : 'demote'
 
   let msgAccion = action === 'promote'
-   ? `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ ⚡ *PROMOCIÓN*
+  ? `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
+│ 👑 *PROMOCIÓN*
 │
-│ 👑 *@${user.split('@')[0]} ahora es Administrador*
-│ 🌙 *Acción por:* @${m.sender.split('@')[0]}
+│ 😼 *@${user.split('@')[0]} ahora es Administrador*
+│ 🐾 *Acción por:* @${m.sender.split('@')[0]}
 │
-│ > *“El trueno le otorga poder”*
+│ > *“Lu le da la llave de la casita”*
 ╰─────────────────❒`
-    : `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ ⛈️ *DEGRADACIÓN*
+    : `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
+│ 🗑️ *DEGRADACIÓN*
 │
-│ 🛡️ *@${user.split('@')[0]} ya no es Administrador*
-│ 🌙 *Acción por:* @${m.sender.split('@')[0]}
+│ 😿 *@${user.split('@')[0]} ya no es Administrador*
+│ 🐾 *Acción por:* @${m.sender.split('@')[0]}
 │
-│ > *“El rayo retira el mando”*
+│ > *“Lu le quitó las patitas del mando”*
 ╰─────────────────❒`
 
   await conn.groupParticipantsUpdate(m.chat, [user], action)
