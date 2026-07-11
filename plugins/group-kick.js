690435464c46ed2dc74b93fbbc3ec42d549449dd
YALLICO OWNER <yallico2024@gmail.com>
2026-07-11 14:26:40 -0500

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
     let mentionedJid = m.mentionedJid && m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : null
 
-    if (!mentionedJid) return conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+    if (!mentionedJid) return conn.reply(m.chat, `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
 │ 🚮 *EXPULSAR USUARIO*
 │
-│ ⚡ *Menciona a un usuario o responde a un mensaje*
+│ 😼 *Menciona a un usuario o responde a un mensaje*
 ╰─────────────────❒`, m)
 
     try {
@@ -12,35 +12,35 @@ let handler = async (m, { conn, participants, usedPrefix, command }) => {
         let ownerGroup = groupMetadata.owner || m.chat.split`-`[0] + '@s.whatsapp.net'
         let ownerBot = global.owner[0][0] + '@s.whatsapp.net'
 
-        if (mentionedJid === conn.user.jid) return conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        if (mentionedJid === conn.user.jid) return conn.reply(m.chat, `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
 │ ❌ *ERROR*
 │
-│ ⚡ *No puedo expulsarme a mi mismo*
+│ 😿 *Lu no puede expulsarse sola*
 ╰─────────────────❒`, m)
-        if (mentionedJid === ownerGroup) return conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        if (mentionedJid === ownerGroup) return conn.reply(m.chat, `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
 │ ❌ *ERROR*
 │
-│ ⚡ *No puedo expulsar al creador del grupo*
+│ 😿 *Lu no puede expulsar al dueño de la casita*
 ╰─────────────────❒`, m)
-        if (mentionedJid === ownerBot) return conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        if (mentionedJid === ownerBot) return conn.reply(m.chat, `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
 │ ❌ *ERROR*
 │
-│ ⚡ *No puedo expulsar al dueño del bot*
+│ 😿 *Lu no puede expulsar a su humano*
 ╰─────────────────❒`, m)
 
         await conn.groupParticipantsUpdate(m.chat, [mentionedJid], 'remove')
-        conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        conn.reply(m.chat, `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
 │ 🚮 *USUARIO EXPULSADO*
 │
-│ ⚡ *@${mentionedJid.split('@')[0]} fue sacado del grupo*
-│ 🌙 *Juicio del trueno ejecutado*
+│ 😼 *@${mentionedJid.split('@')[0]} fue sacado de la casita*
+│ 🐾 *Lu lo sacó por travieso*
 ╰─────────────────❒`, m, { mentions: [mentionedJid] })
     } catch (e) {
-        conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ ⛈️ *ERROR*
+        conn.reply(m.chat, `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
+│ 😿 *ERROR*
 │
-│ ⚡ *Ocurrió un problema*
-│ 🌙 *${e.message}*
+│ 🐾 *Ocurrió un problema*
+│ 😼 *${e.message}*
 ╰─────────────────❒`, m)
     }
 }
