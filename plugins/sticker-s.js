473acbbb4ace24ffc5477692c0bc6c7f4d74a87a
YALLICO OWNER <yallico2024@gmail.com>
2026-07-11 10:45:57 -0500

    Actualizar sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -1,27 +1,22 @@
 import { sticker } from '../lib/sticker.js'
-
 let handler = async (m, { conn, usedPrefix, command }) => {
-    let q = m.quoted ? m.quoted : m
+    let q = m.quoted? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ 🎭 *STICKER*
+    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
+│ 🎨 *STICKER*
 │
 │ ⚠️ *Responde a una imagen, video o gif*
-│ 🌙 *Para convertirlo en sticker*
+│ 🐾 *Para convertirlo en sticker*
 ╰─────────────────❒`)
-    
     let img = await q.download()
-    let stiker = await sticker(img, false, 'Team Nightwish', 'Whois Yallico')
-    
-    await conn.sendFile(m.chat, stiker, 'sticker.webp', `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ ⚡ *STICKER CREADO*
+    let stiker = await sticker(img, false, 'Bot Lu', 'Whois Yallico')
+    await conn.sendFile(m.chat, stiker, 'sticker.webp', `╭─🐱 *『 𝐁𝐎𝐓 𝐋𝐔 』* 🐱
+│ ✨ *STICKER CREADO*
 │
-│ > *El trueno en forma de sticker*
+│ > *Lu dejó su patita aquí* 🐾
 ╰─────────────────❒`, m)
 }
-
 handler.help = ['s']
 handler.tags = ['sticker']
 handler.command = ['s', 'sticker', 'stiker']
-
 export default handler
